eb77d80c1183a5f8580c9eee2d986c94c46aae59
42784XM <3485376023@qq.com>
2025-05-04 18:41:09 +0800

    增加休息段，替换时长硬编码到类变量

--- src/main/java/github/axolotl/timeclock/Main.java
+++ src/main/java/github/axolotl/timeclock/Main.java
@@ -14,15 +14,21 @@ public class Main {
     private static long playTime = System.currentTimeMillis();//响起铃声的时间
     private static final Random rand = new Random();
 
+    private static final int relaxTime = 15_000;
+    private static final long studyTimeMax = 6 * 60 * 1000;
+    private static final long studyTimeMin = 3 * 60 * 1000;
+
     @SneakyThrows
     public static void main(String[] args) {
         while (true) {
             if (System.currentTimeMillis() > playTime) {
-                SoundPlayUtil.playerSound("./sounds/ringing_short.mp3");
-                playTime += (long) (rand.nextDouble(3, 6) *60.0* 1000);
+                SoundPlayUtil.playerSound("./sounds/Speech On.mp3");
+                Thread.sleep(relaxTime);//中途休息
+                SoundPlayUtil.playerSound("./sounds/Speech Off.mp3");
+                playTime += rand.nextLong(studyTimeMin, studyTimeMax) + relaxTime;//下一次响铃时间
             }
-            Thread.sleep(500);
-            System.out.printf("剩余时间: %ds\n",( playTime - System.currentTimeMillis())/1000);
+            Thread.sleep(1200);
+            System.out.printf("剩余时间: %ds\n", (playTime - System.currentTimeMillis()) / 1000);
         }
     }
 }
