3ea0939146d4add10637ae897b526185fcfe47c9
42784XM <3485376023@qq.com>
2025-05-05 13:28:20 +0800

    增加休息段，替换时长硬编码到类变量

--- src/main/java/github/axolotl/timeclock/Main.java
+++ src/main/java/github/axolotl/timeclock/Main.java
@@ -3,7 +3,6 @@ package github.axolotl.timeclock;
 import dczx.axolotl.util.SoundPlayUtil;
 import lombok.SneakyThrows;
 
-import java.sql.Time;
 import java.util.Random;
 
 /**
@@ -14,17 +13,21 @@ public class Main {
     private static long playTime = System.currentTimeMillis();//响起铃声的时间
     private static final Random rand = new Random();
 
-    private static final int relaxTime = 15_000;
+    private static final int relaxTime = 15_000;//休息时间 单位ms
+    //最长最短的随机学习时长 单位ms
     private static final long studyTimeMax = 6 * 60 * 1000;
     private static final long studyTimeMin = 3 * 60 * 1000;
+    //开始和结束休息的提示音 仅支持mp3
+    private static final String startRelaxMediaPath = "./sounds/Speech On.mp3";
+    private static final String stopRelaxMediaPath = "./sounds/Speech Off.mp3";
 
     @SneakyThrows
     public static void main(String[] args) {
         while (true) {
             if (System.currentTimeMillis() > playTime) {
-                SoundPlayUtil.playerSound("./sounds/Speech On.mp3");
+                SoundPlayUtil.playerSound(startRelaxMediaPath);
                 Thread.sleep(relaxTime);//中途休息
-                SoundPlayUtil.playerSound("./sounds/Speech Off.mp3");
+                SoundPlayUtil.playerSound(stopRelaxMediaPath);
                 playTime += rand.nextLong(studyTimeMin, studyTimeMax) + relaxTime;//下一次响铃时间
             }
             Thread.sleep(1200);
